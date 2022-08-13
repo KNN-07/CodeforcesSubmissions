3c2940222254f2d5db7d4f76b358d31f0c8d7c5e
Nguyen Khac Trung Kien <>
2022-08-13 16:19:00 +0000

    Add solution for problem `A - Wrong Subtraction` Link: https://codeforces.com/contest/977/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/977/submission/168167476

---
+++ codeforces/977/A.cpp
@@ -0,0 +1,47 @@
+/*
+Templete by norman/KNN-07
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define el endl
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define fi first
+#define nd second
+#define forinc(i, a, b) for (int i = a; i <= b; i++)
+#define fordec(i, a, b) for (int i = a; i >= b; i--)
+#define mms(a, v) memset(a, v, sizeof(a))
+#define lwb(a, v) lower_bound(a.begin(), a.end(), v)
+#define upb(a, v) upper_bound(a.begin(), a.end(), v)
+typedef long long ll;
+typedef long double ldb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef vector<bool> vb;
+typedef unordered_set<char> chrset;
+
+int main()
+{
+    ll inp;
+    int k;
+    cin >> inp >> k;
+    for (int i = 0; i < k; i++)
+    {
+        if (inp % 10 != 0)
+        {
+            inp--;
+        }
+        else
+        {
+            inp /= 10;
+        }
+    }
+
+    cout << inp;
+}
