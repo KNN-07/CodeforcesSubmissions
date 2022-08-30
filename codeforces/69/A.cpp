2c2614e0fc637c20da0d45dd9984ca52764a0a7f
Nguyen Khac Trung Kien <>
2022-08-30 13:44:00 +0000

    Add solution for problem `A - Young Physicist` Link: https://codeforces.com/contest/69/problem/A Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/69/submission/170165258

--- codeforces/69/A.cpp
+++ codeforces/69/A.cpp
@@ -1,16 +1,57 @@
+/*
+Templete by norman/KNN-07
+*/
 #include <bits/stdc++.h>
-
 using namespace std;
+#define el endl
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define fi first
+#define nd second
+#define forinc(i, a, b) for (int i = a; i <= b; i++)
+#define fordec(i, a, b) for (int i = a; i >= b; i--)
+#define mms(a, v) memset(a, v, sizeof(a))
+#define lwb(a, v) lower_bound(a.begin(), a.end(), v)
+#define upb(a, v) upper_bound(a.begin(), a.end(), v)
+typedef long long ll;
+typedef long double ldb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef vector<bool> vb;
+typedef unordered_set<char> chrset;
 
 int main()
 {
-    long long n;
+    // stdin
+    //  freopen("<file name insert>.inp", "r", stdin);
+    //  freopen("<file name insert>.out", "w", stdout);
+    //  Code here
+    int n;
     cin >> n;
-    long long sx=0,sy=0,sz=0;
-    while (n--){
-        long long x,y,z;
-        cin >> x>>y>>z;
-        sx+=x,sy+=y,sz+=z;
+    int pos[n][3];
+    for (int i = 0; i < n * 3; i++)
+    {
+        cin >> pos[i][0];
+        cin >> pos[i][1];
+        cin >> pos[i][2];
     }
-    cout << ((sx == 0&&sy==0&&sz==0)? "YES" : "NO");
+    int sum1 = 0, sum2 = 0, sum3 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum1 += pos[i][0];
+        sum2 += pos[i][1];
+        sum3 += pos[i][2];
+    }
+    if (sum1 == 0 && sum2 == 0 && sum3 == 0)
+    {
+        cout << "YES";
+    }
+    else
+        cout << "NO";
 }
