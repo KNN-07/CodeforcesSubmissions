3c1c61ed9fc7efb48dd98719738ed28e43307ef9
Nguyen Khac Trung Kien <>
2022-09-29 10:08:00 +0000

    Add solution for problem `A - Consecutive Sum` Link: https://codeforces.com/contest/1733/problem/A Tags: greedy, sortings, *800 Ref: https://codeforces.com/contest/1733/submission/173905927

---
+++ codeforces/1733/A.cpp
@@ -0,0 +1,64 @@
+/*
+Templete by norman/KNN-07
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define el endl
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define fi first
+#define nd second
+#define forinc(i, a, b) for (int i = a; i <= b; i++)
+#define fordec(i, a, b) for (int i = a; i >= b; i--)
+#define mms(a, v) memset(a, v, sizeof(a))
+#define lwb(a, v) lower_bound(a.begin(), a.end(), v)
+#define upb(a, v) upper_bound(a.begin(), a.end(), v)
+typedef long long ll;
+typedef long double ldb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef vector<bool> vb;
+typedef unordered_set<char> chrset;
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    int in[n];
+    mms(in, 0);
+    int a;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a;
+        in[i % k] = max(in[i % k], a);
+    }
+    ll s = 0;
+    for (int i = 0; i < k; i++)
+    {
+        s += in[i];
+    }
+    cout << s;
+}
+
+int main()
+{
+    // stdin
+    //  freopen("<file name insert>.inp", "r", stdin);
+    //  freopen("<file name insert>.out", "w", stdout);
+    //  Code here
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        solve();
+        cout << el;
+    }
+}
