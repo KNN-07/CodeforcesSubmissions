848133cbb77a598ccd8a24f5ef232f5224d675fa
Nguyen Khac Trung Kien <>
2022-10-03 05:12:00 +0000

    Add solution for problem `A - Working Week` Link: https://codeforces.com/contest/1735/problem/A Tags: constructive algorithms, greedy, math, *800 Ref: https://codeforces.com/contest/1735/submission/174469395

---
+++ codeforces/1735/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+	long long n;
+	cin >> n;
+	cout << (n-3)/3-1;
+}
+int main(){
+	int n;
+	cin >> n;
+	while (n--){
+		solve();
+		cout << "\n";
+	}
+}
