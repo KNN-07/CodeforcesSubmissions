ea6fb88251cb2c1f906dd3bf916c321283ce09fb
Nguyen Khac Trung Kien <>
2022-10-18 13:39:00 +0000

    Add solution for problem `D - Deleting Divisors` Link: https://codeforces.com/contest/1537/problem/D Tags: games, math, number theory, *1700 Ref: https://codeforces.com/contest/1537/submission/176887226

---
+++ codeforces/1537/D.cpp
@@ -0,0 +1,68 @@
+/*
+Templete by norman/KNN-07
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define el endl
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define fi first
+#define nd second
+#define forinc(i, a, b) for (int i = a; i <= b; i++)
+#define fordec(i, a, b) for (int i = a; i >= b; i--)
+#define mms(a, v) memset(a, v, sizeof(a))
+#define lwb(a, v) lower_bound(a.begin(), a.end(), v)
+#define upb(a, v) upper_bound(a.begin(), a.end(), v)
+typedef long long ll;
+typedef long double ldb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef vector<bool> vb;
+typedef unordered_set<char> chrset;
+const bool tc = true;
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n & 1)
+    {
+        cout << "Bob";
+        return;
+    }
+    ll p2 = 0;
+    while (!(n & 1))
+    {
+        n /= 2;
+        p2++;
+    }
+    if (n >= 2 || !(p2 & 1))
+        cout << "Alice";
+    else cout << "Bob";
+}
+
+int main()
+{
+    // stdin
+    //  freopen("<file name insert>.inp", "r", stdin);
+    //  freopen("<file name insert>.out", "w", stdout);
+    //  Code here
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int tsc = 1;
+    if (tc)
+    {
+        cin >> tsc;
+    }
+    while (tsc--)
+    {
+        solve();
+        cout << el;
+    }
+}
