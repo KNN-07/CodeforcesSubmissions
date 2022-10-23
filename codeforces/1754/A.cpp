621b01334ff426c6a2aa9aa3aeada1d4c8aee793
Nguyen Khac Trung Kien <>
2022-10-23 14:16:00 +0000

    Add solution for problem `A - Technical Support` Link: https://codeforces.com/contest/1754/problem/A Tags: greedy, *800 Ref: https://codeforces.com/contest/1754/submission/177656621

---
+++ codeforces/1754/A.cpp
@@ -0,0 +1,65 @@
+/*
+Templete by norman/KNN-07
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define el endl
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define fi first
+#define nd second
+#define forinc(i, a, b) for (int i = a; i <= b; i++)
+#define fordec(i, a, b) for (int i = a; i >= b; i--)
+#define mms(a, v) memset(a, v, sizeof(a))
+#define lwb(a, v) lower_bound(a.begin(), a.end(), v)
+#define upb(a, v) upper_bound(a.begin(), a.end(), v)
+typedef long long ll;
+typedef long double ldb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef vector<bool> vb;
+typedef unordered_set<char> chrset;
+const bool tc = true;
+void solve()
+{
+    int n;
+    cin >> n;
+    int q = 0;
+    for (int i = 0; i < n; i++)
+    {
+        char t;
+        cin >> t;
+        if (t == 'Q')
+            q++;
+        else
+            q = max(0, (q - 1));
+    }
+    cout << (q != 0 ? "No" : "Yes");
+}
+
+int main()
+{
+    // stdin
+    //  freopen("<file name insert>.inp", "r", stdin);
+    //  freopen("<file name insert>.out", "w", stdout);
+    //  Code here
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int tsc = 1;
+    if (tc)
+    {
+        cin >> tsc;
+    }
+    while (tsc--)
+    {
+        solve();
+        cout << el;
+    }
+}
