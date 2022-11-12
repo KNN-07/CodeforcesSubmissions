74048aa8847bf17d1aeeb9ac080e6552b11fd855
Nguyen Khac Trung Kien <>
2022-11-12 17:13:00 +0000

    Add solution for problem `A - The Ultimate Square` Link: https://codeforces.com/contest/1748/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1748/submission/180661260

---
+++ codeforces/1748/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--){
+        ll a;
+        cin >> a;
+        cout << (a+1)/2 << "\n";
+    }
+}
