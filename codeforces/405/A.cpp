2cd8d3b1061729868694094116615382b4c560aa
Nguyen Khac Trung Kien <>
2022-11-20 02:45:00 +0000

    Add solution for problem `A - Gravity Flip` Link: https://codeforces.com/contest/405/problem/A Tags: greedy, implementation, sortings, *900 Ref: https://codeforces.com/contest/405/submission/181677274

---
+++ codeforces/405/A.cpp
@@ -0,0 +1,63 @@
+/*
+Templete by norman/KNN-07
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define el endl
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define fi first
+#define nd second
+#define forinc(i, a, b) for (int i = a; i <= b; i++)
+#define fordec(i, a, b) for (int i = a; i >= b; i--)
+#define mms(a, v) memset(a, v, sizeof(a))
+#define lwb(a, v) lower_bound(a.begin(), a.end(), v)
+#define upb(a, v) upper_bound(a.begin(), a.end(), v)
+typedef long long ll;
+typedef long double ldb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef vector<bool> vb;
+typedef unordered_set<char> chrset;
+const string taskname = "";
+const bool tc = false;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+    for (auto &x : a)
+        cin >> x;
+    sort(a.begin(), a.end());
+    for (auto x : a)
+        cout << x << " ";
+}
+
+int main()
+{
+    if (fopen((taskname + ".inp").c_str(), "r"))
+    {
+        freopen((taskname + ".inp").c_str(), "r", stdin);
+        freopen((taskname + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int tsc = 1;
+    if (tc)
+    {
+        cin >> tsc;
+    }
+    while (tsc--)
+    {
+        solve();
+        cout << el;
+    }
+}
