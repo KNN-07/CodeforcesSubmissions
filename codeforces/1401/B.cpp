42e5d780f1385ac9cfcfed47ff654aba8e58b81f
Nguyen Khac Trung Kien <>
2022-11-22 14:41:00 +0000

    Add solution for problem `B - Ternary Sequence` Link: https://codeforces.com/contest/1401/problem/B Tags: constructive algorithms, greedy, math, *1100 Ref: https://codeforces.com/contest/1401/submission/182141331

---
+++ codeforces/1401/B.cpp
@@ -0,0 +1,67 @@
+/*
+Templete by norman/KNN-07
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define el endl
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define fi first
+#define nd second
+#define forinc(i, a, b) for (int i = a; i <= b; i++)
+#define fordec(i, a, b) for (int i = a; i >= b; i--)
+#define mms(a, v) memset(a, v, sizeof(a))
+#define lwb(a, v) lower_bound(a.begin(), a.end(), v)
+#define upb(a, v) upper_bound(a.begin(), a.end(), v)
+typedef long long ll;
+typedef long double ldb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef vector<bool> vb;
+typedef unordered_set<char> chrset;
+const string taskname = "";
+const bool tc = true;
+
+void solve()
+{
+    int x1, x2, y1, y2, z1, z2;
+    cin >> x1 >> y1 >> z1 >> x2 >> y2 >> z2;
+    int a;
+    a = min(x1, z2); // Minimum 0,2 pair;
+    x1 -= a, z2 -= a;
+    a = min(y1, x2); // Minimum 1,0 pair;
+    y1 -= a, x2 -= a;
+    a = min(z1, y2); // Maximum 2,1 pair;
+    z1 -= a, y2 -= a;
+    ll sum = 2 * a;
+    sum -= 2 * min(y1, z2); // Remove (1,2) Pair
+    cout << sum;
+}
+
+int main()
+{
+    if (fopen((taskname + ".inp").c_str(), "r"))
+    {
+        freopen((taskname + ".inp").c_str(), "r", stdin);
+        freopen((taskname + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int tsc = 1;
+    if (tc)
+    {
+        cin >> tsc;
+    }
+    while (tsc--)
+    {
+        solve();
+        cout << el;
+    }
+}
