e77cae3c3c707e75413f3d0953249501be5c2238
Nguyen Khac Trung Kien <>
2022-11-25 18:10:00 +0000

    Add solution for problem `C - Almost All Multiples` Link: https://codeforces.com/contest/1758/problem/C Tags: greedy, number theory, *1400 Ref: https://codeforces.com/contest/1758/submission/182546483

---
+++ codeforces/1758/C.cpp
@@ -0,0 +1,73 @@
+/*
+Templete by norman/KNN-07
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define el endl
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define fi first
+#define nd second
+#define forinc(i, a, b) for (int i = a; i <= b; i++)
+#define fordec(i, a, b) for (int i = a; i >= b; i--)
+#define mms(a, v) memset(a, v, sizeof(a))
+#define lwb(a, v) lower_bound(a.begin(), a.end(), v)
+#define upb(a, v) upper_bound(a.begin(), a.end(), v)
+typedef long long ll;
+typedef long double ldb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef vector<bool> vb;
+typedef unordered_set<char> chrset;
+const string taskname = "";
+const bool tc = true;
+
+void solve()
+{
+    int n, x;
+    cin >> n >> x;
+    if ((n % x))
+    {
+        cout << -1;
+        return;
+    }
+    vi a(n);
+    a[0] = x;
+    iota(a.begin() + 1, a.end(), 2);
+    a[x - 1] = n;
+    a[n - 1] = 1;
+    for (int i = 2; i < n; i++)
+        for (int j = i * 2; j < n; j += i)
+            if (a[i - 1] % j == 0 and a[i - 1] > a[j - 1])
+                swap(a[i - 1], a[j - 1]);
+    for (auto x : a)
+        cout << x << ' ';
+}
+
+int main()
+{
+    if (fopen((taskname + ".inp").c_str(), "r"))
+    {
+        freopen((taskname + ".inp").c_str(), "r", stdin);
+        freopen((taskname + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int tsc = 1;
+    if (tc)
+    {
+        cin >> tsc;
+    }
+    while (tsc--)
+    {
+        solve();
+        cout << el;
+    }
+}
