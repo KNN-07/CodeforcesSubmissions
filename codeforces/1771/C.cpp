7b100118f2effbfb4d3f15c1fc798725774b83e9
Nguyen Khac Trung Kien <trungkiennguyenkhac007@gmail.com>
2022-12-12 06:01:00 +0700

    Add solution for problem `C - Hossam and Trainees` Link: https://codeforces.com/contest/1771/problem/C Tags: greedy, math, number theory, *1600 Ref: https://codeforces.com/contest/1771/submission/184827912

---
+++ codeforces/1771/C.cpp
@@ -0,0 +1,141 @@
+/*
+Templete by norman/KNN-07
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define el endl
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define fi first
+#define nd second
+#define forinc(i, a, b) for (int i = a; i <= b; i++)
+#define fordec(i, a, b) for (int i = a; i >= b; i--)
+#define alle(x) (x).begin(), (x).end()
+#define ralle(x) (x).rbegin(), (x).rend()
+#define mms(a, v) memset(a, v, sizeof(a))
+#define lwb(a, v) lower_bound(a.begin(), a.end(), v)
+#define upb(a, v) upper_bound(a.begin(), a.end(), v)
+typedef long long ll;
+typedef long double ldb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef vector<bool> vb;
+typedef unordered_set<char> chrset;
+const string taskname = "";
+const bool tc = true;
+
+// tourist templete lol
+
+typedef unsigned long long ull;
+ull modmul(ull a, ull b, ull M)
+{
+    ll ret = a * b - M * ull(1.L / M * a * b);
+    return ret + M * (ret < 0) - M * (ret >= (ll)M);
+}
+ull modpow(ull b, ull e, ull mod)
+{
+    ull ans = 1;
+    for (; e; b = modmul(b, b, mod), e /= 2)
+        if (e & 1)
+            ans = modmul(ans, b, mod);
+    return ans;
+}
+
+bool isPrime(ull n)
+{
+    if (n < 2 || n % 6 % 4 != 1)
+        return (n | 1) == 3;
+    ull A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022},
+        s = __builtin_ctzll(n - 1), d = n >> s;
+    for (ull a : A)
+    { // ^ count trailing zeroes
+        ull p = modpow(a % n, d, n), i = s;
+        while (p != 1 && p != n - 1 && a % n && i--)
+            p = modmul(p, p, n);
+        if (p != n - 1 && i != s)
+            return 0;
+    }
+    return 1;
+}
+
+ull pollard(ull n)
+{
+    auto f = [n](ull x)
+    { return modmul(x, x, n) + 1; };
+    ull x = 0, y = 0, t = 30, prd = 2, i = 1, q;
+    while (t++ % 40 || __gcd(prd, n) == 1)
+    {
+        if (x == y)
+            x = ++i, y = f(x);
+        if ((q = modmul(prd, max(x, y) - min(x, y), n)))
+            prd = q;
+        x = f(x), y = f(f(y));
+    }
+    return __gcd(prd, n);
+}
+vector<ull> factor(ull n)
+{
+    if (n == 1)
+        return {};
+    if (isPrime(n))
+        return {n};
+    ull x = pollard(n);
+    auto l = factor(x), r = factor(n / x);
+    l.insert(l.end(), alle(r));
+    return l;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi arr(n);
+    set<ull> vis;
+    for (auto &x : arr)
+    {
+        cin >> x;
+    }
+    for (int x : arr)
+    {
+        auto fac = factor(x);
+        for (auto x : fac)
+        {
+            if (vis.count(x))
+            {
+                cout << "YES";
+                return;
+            }
+        }
+        for (auto x : fac)
+            vis.insert(x);
+    }
+    cout << "NO";
+}
+
+int main()
+{
+    if (fopen((taskname + ".inp").c_str(), "r"))
+    {
+        freopen((taskname + ".inp").c_str(), "r", stdin);
+        freopen((taskname + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int tsc = 1;
+    if (tc)
+    {
+        cin >> tsc;
+    }
+    while (tsc--)
+    {
+        solve();
+        cout << el;
+    }
+}
