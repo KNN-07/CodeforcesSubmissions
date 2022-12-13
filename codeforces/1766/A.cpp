c618cc7162c8666dcb4fdf9e83fc14b9b6545138
Nguyen Khac Trung Kien <>
2022-12-13 00:44:00 +0000

    Add solution for problem `A - Extremely Round` Link: https://codeforces.com/contest/1766/problem/A Tags: brute force, implementation, *800 Ref: https://codeforces.com/contest/1766/submission/185020200

---
+++ codeforces/1766/A.cpp
@@ -0,0 +1,74 @@
+/*
+Templete by norman/KNN-07
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define el endl
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define fi first
+#define nd second
+#define forinc(i, a, b) for (int i = a; i <= b; i++)
+#define fordec(i, a, b) for (int i = a; i >= b; i--)
+#define mms(a, v) memset(a, v, sizeof(a))
+#define lwb(a, v) lower_bound(a.begin(), a.end(), v)
+#define upb(a, v) upper_bound(a.begin(), a.end(), v)
+typedef long long ll;
+typedef long double ldb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef vector<bool> vb;
+typedef unordered_set<char> chrset;
+const bool tc = true;
+const string taskname = "";
+
+const ll MAXN = 1e6 + 1;
+ll p10[MAXN];
+
+void init()
+{
+    p10[0] = 1;
+    for (int i = 1; i <= MAXN - 1; i++)
+    {
+        p10[i] = p10[i - 1] * 10;
+    }
+}
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    int i = 0;
+    for (i = 0; p10[i + 1] < n; i++)
+        continue;
+    cout << n / p10[i] + i * 9;
+}
+
+int main()
+{
+    if (fopen((taskname + ".inp").c_str(), "r"))
+    {
+        freopen((taskname + ".inp").c_str(), "r", stdin);
+        freopen((taskname + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    init();
+    int tsc = 1;
+    if (tc)
+    {
+        cin >> tsc;
+    }
+    while (tsc--)
+    {
+        solve();
+        cout << el;
+    }
+}
