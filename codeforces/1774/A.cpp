0dd3184364fc6de723f2e6f41d7411ad9e2f8be3
Nguyen Khac Trung Kien <trungkiennguyenkhac007@gmail.com>
2022-12-18 09:14:00 +0700

    Add solution for problem `A - Add Plus Minus Sign` Link: https://codeforces.com/contest/1774/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1774/submission/185740419

---
+++ codeforces/1774/A.cpp
@@ -0,0 +1,82 @@
+/*
+Templete by norman/KNN-07
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define el endl
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define fi first
+#define nd second
+#define forinc(i, a, b) for (int i = a; i <= b; i++)
+#define fordec(i, a, b) for (int i = a; i >= b; i--)
+#define alle(x) (x).begin(), (x).end()
+#define ralle(x) (x).rbegin(), (x).rend()
+#define mms(a, v) memset(a, v, sizeof(a))
+#define lwb(a, v) lower_bound(a.begin(), a.end(), v)
+#define upb(a, v) upper_bound(a.begin(), a.end(), v)
+typedef long long ll;
+typedef long double ldb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef vector<bool> vb;
+typedef unordered_set<char> chrset;
+const string taskname = "";
+const bool tc = true;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int tr = s[0] - '0';
+    for (int i = 1; i < n; i++)
+    {
+        if (s[i] == '0')
+        {
+            cout << '+';
+        }
+        else
+        {
+            if (tr > 0)
+            {
+                cout << '-';
+                tr--;
+            }
+            else
+            {
+                cout << '+';
+                tr++;
+            }
+        }
+    }
+}
+
+int main()
+{
+    if (fopen((taskname + ".inp").c_str(), "r"))
+    {
+        freopen((taskname + ".inp").c_str(), "r", stdin);
+        freopen((taskname + ".out").c_str(), "w", stdout);
+    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int tsc = 1;
+    if (tc)
+    {
+        cin >> tsc;
+    }
+    while (tsc--)
+    {
+        solve();
+        cout << el;
+    }
+}
